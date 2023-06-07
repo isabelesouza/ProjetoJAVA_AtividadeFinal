da0bd89419dbfbbf32cf428e6c4c67c312bc260c
Isabele Souza <isabelesouzam@gmail.com>
2023-06-07 04:31:57 -0300

    aprimorando classe restaurante

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,7 +1,16 @@
-public class Restaurante {
-    public static void main(String[] args) {
-        Pedido pedido = new Pedido();
-        pedido.fazerPedido("Pizza");
-    }
-}
+import java.util.Scanner;
 
+public class Restaurante {
+	 public static void main(String[] args) {
+		 
+		  	Scanner scanner = new Scanner(System.in);
+	        System.out.println("Bem-vindo ao nosso Restaurante!");
+
+	        System.out.print("Digite o nome da comida: ");
+	        String comida = scanner.nextLine();
+	        
+	      	Pedido pedido = new Pedido();
+	        pedido.fazerPedido(comida);  
+
+}
+}
