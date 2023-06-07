692b65fe79a7b51f9eb0de26aab4c76e2a0a772e
Isabele Souza <isabelesouzam@gmail.com>
2023-06-07 04:18:41 -0300

    classe pedido com métodos

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,6 +1,21 @@
 public class Pedido {
-    public void atendeCliente() {
-        
-        System.out.println("O cliente atendido foi atendido!");
+
+    public void fazerPedido(String comida) {
+        if (comida.equals("Pizza")) {
+            prepararPizza();
+        } else if (comida.equals("Hambúrguer")) {
+
+            prepararHamburguer();
+        } else {
+            System.out.println("Desculpe, não temos esse prato no cardápio.");
+        }
+    }
+
+    private void prepararPizza() {
+        System.out.println("Preparando pizza...");
+    }
+
+    private void prepararHamburguer() {
+        System.out.println("Preparando hambúrguer...");
     }
 }
