a516aa9ed960dd2b9c1ef20fb76655524f28724b
Isabele Souza <isabelesouzam@gmail.com>
2023-06-07 03:35:35 -0300

    início

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,6 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("teste");
+public class Restaurante {
+    public void fazerPedido() {
+        // Implementação da função fazerPedido
+        System.out.println("Pedido feito com sucesso!");
+    }
+
+    public void prepararComida() {
+        // Implementação da função prepararComida
+        System.out.println("Comida preparada!");
     }
 }
+
